eda74f46baf8c4a995595654ad9554187cad89e6
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-02 09:38:23 +0200

    Updated the TransactionDTO.

--- src/application/dtos/TransactionDTO.ts
+++ src/application/dtos/TransactionDTO.ts
@@ -1,12 +1,14 @@
 ﻿import { UUID } from "node:crypto";
 
 export interface TransactionDTO {
+  id: UUID;
+  workspaceId: UUID;
+  date: Date;
+  category: {
     id: UUID;
-    workspaceId: UUID;
-    date: Date;
-    categoryId: UUID;
-    categoryName: string;
-    amount: number;
-    notes: string;
-    documentationUrl: string;
+    name: string;
+  };
+  amount: number;
+  notes: string;
+  documentationUrl: string;
 }
