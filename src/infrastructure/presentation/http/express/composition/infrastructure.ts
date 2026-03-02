4219ec3a4b3f600be18982272720beab0893ba59
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-02 11:14:02 +0200

    Added the background email sending service, redis otp repository and otp generator.

--- src/infrastructure/presentation/http/express/composition/infrastructure.ts
+++ src/infrastructure/presentation/http/express/composition/infrastructure.ts
@@ -8,18 +8,26 @@ import { AppwriteInviteRepository } from "@appwrite/repositories/InviteRepositor
 import { AppwriteTransactionRepository } from "@appwrite/repositories/TransactionRepository";
 import { AppwriteUserRepository } from "@appwrite/repositories/UserRepository";
 import { AppwriteWorkspaceRepository } from "@appwrite/repositories/WorkspaceRepository";
+import { RedisOtpRepository } from "@infrastructure/persistence/redis/OtpRepository";
+import { DefaultOTPGenerator } from "@infrastructure/services/default-otp-generator";
+import { MockBackgroundEmailSendingService } from "@infrastructure/services/mock-background-email-sending-service";
 import { RandomUUIDGenerator } from "@services/id-generator/IDGenerator";
 import { BcryptPasswordHasher } from "@services/password-hasher/PasswordHasher";
 import { DefaultPasswordPolicy } from "@services/password-policy/PasswordPolicy";
 import { AppwriteTransactionRunner } from "@services/transaction-runner/AppwriteTransactionRunner";
+import { createClient } from "redis";
 
-export function createInfrastructure() {
+export async function createInfrastructure() {
   const db = new AppwriteDatabaseService(
     process.env.APPWRITE_ENDPOINT!,
     process.env.APPWRITE_PROJECT!,
     process.env.APPWRITE_API_KEY!,
     process.env.DATABASE_ID!,
   );
+
+  const redisOtpClient = createClient({
+    url: process.env.REDIS_OTP_REPOSITORY_URL!,
+  });
 
   return {
     repositories: {
@@ -39,6 +47,9 @@ export function createInfrastructure() {
       passwordPolicy: new DefaultPasswordPolicy(),
       idGenerator: new RandomUUIDGenerator(),
       transactionRunner: new AppwriteTransactionRunner(db),
+      otpGenerator: new DefaultOTPGenerator(),
+      backgroundEmailSendingService: new MockBackgroundEmailSendingService(),
+      otpRepository: new RedisOtpRepository(redisOtpClient),
     },
   };
 }
