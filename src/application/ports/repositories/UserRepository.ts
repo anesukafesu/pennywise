e9b4e759126a6a84d2f49deb38dc0905eefc74cb
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-02 08:44:00 +0200

    Added a getManyByIds method.

--- src/application/ports/repositories/UserRepository.ts
+++ src/application/ports/repositories/UserRepository.ts
@@ -1,20 +1,20 @@
 ﻿import { TransactionContext } from "@application/ports/services/TransactionRunner";
 import { User } from "@entities/User";
+import { UUID } from "node:crypto";
 
 export interface UserRepository {
   createOne(user: User, tx?: TransactionContext): Promise<void>;
 
-  getOneById(
-    userId: string,
-    tx?: TransactionContext,
-  ): Promise<User | undefined>;
+  getOneById(userId: UUID, tx?: TransactionContext): Promise<User | undefined>;
 
   getOneByEmail(
     email: string,
     tx?: TransactionContext,
   ): Promise<User | undefined>;
 
+  getManyByIds(ids: UUID[], tx?: TransactionContext): Promise<User[]>;
+
   updateOne(user: User, tx?: TransactionContext): Promise<void>;
 
-  deleteOneById(id: string, tx?: TransactionContext): Promise<void>;
+  deleteOneById(id: UUID, tx?: TransactionContext): Promise<void>;
 }
