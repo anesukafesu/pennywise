f51abca9c8a10a085fe9987d1054c6a5623762f9
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-02 09:08:43 +0200

    Generalised the function from working with the Actor object to any object with an id.

---
+++ src/application/guards/ensureUserHasNoExistingCollaboration.ts
@@ -0,0 +1,21 @@
+﻿import { CollaborationRepository } from "@application/ports/repositories/CollaborationRepository";
+import { ResourceConflict } from "@application/errors/ResourceConflict";
+import { UUID } from "node:crypto";
+
+export async function ensureUserHasNoExistingCollaboration(
+  user: { id: UUID },
+  workspaceId: UUID,
+  collaborationRepository: CollaborationRepository,
+) {
+  const existingCollaboration =
+    await collaborationRepository.getOneByUserIdAndWorkspaceId(
+      user.id,
+      workspaceId,
+    );
+
+  if (existingCollaboration) {
+    throw new ResourceConflict(
+      `The user (${user.id}) is already a collaborator in workspace (${workspaceId}).`,
+    );
+  }
+}
---
+++ src/application/guards/ensureUserHasNoExistingInvite.ts
@@ -0,0 +1,20 @@
+﻿import { InviteRepository } from "@application/ports/repositories/InviteRepository";
+import { ResourceConflict } from "@application/errors/ResourceConflict";
+import { UUID } from "node:crypto";
+
+export async function ensureUserHasNoExistingInvite(
+  user: { id: UUID },
+  workspaceId: UUID,
+  inviteRepository: InviteRepository,
+) {
+  const existingInvite = await inviteRepository.getOneByUserIdAndWorkspaceId(
+    user.id,
+    workspaceId,
+  );
+
+  if (existingInvite) {
+    throw new ResourceConflict(
+      `User already has an invite to: ${workspaceId}.`,
+    );
+  }
+}
