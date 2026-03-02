2d4ec1fd0ad2218ad05a604e1a898769165dae7b
Anesu Kafesu <a.kafesu@alustudent.com>
2026-03-02 09:39:01 +0200

    Updated the function to create dtos that match the new transaction dto interface.

--- src/application/mappers/mapTransactionsToDTOs.ts
+++ src/application/mappers/mapTransactionsToDTOs.ts
@@ -4,30 +4,36 @@ import { Transaction } from "@entities/Transaction";
 import { CategoryRepository } from "@application/ports/repositories/CategoryRepository";
 
 export async function mapTransactionsToDTOs(
-    transactions: Transaction[],
-    categoryRepository: CategoryRepository
+  transactions: Transaction[],
+  categoryRepository: CategoryRepository,
 ): Promise<TransactionDTO[]> {
+  const categoryIds = [...new Set(transactions.map((t) => t.categoryId))];
+  const transactionCategories =
+    await categoryRepository.getManyByIds(categoryIds);
+  const categoriesMap = new Map(
+    transactionCategories.map((c) => [c.id, c.name]),
+  );
 
-    const categoryIds = [...new Set(transactions.map(t => t.categoryId))];
-    const transactionCategories = await categoryRepository.getManyByIds(categoryIds);
-    const categoriesMap = new Map(transactionCategories.map(c => [c.id, c.name]));
+  return transactions.map((transaction) => {
+    const categoryName = categoriesMap.get(transaction.categoryId);
 
-    return transactions.map(transaction => {
-        const categoryName = categoriesMap.get(transaction.categoryId);
+    if (!categoryName) {
+      throw new NotFound("category", transaction.categoryId);
+    }
 
-        if (!categoryName) {
-            throw new NotFound("category", transaction.categoryId);
-        }
-
-        return {
-            id: transaction.id,
-            workspaceId: transaction.workspaceId,
-            date: transaction.date,
-            categoryId: transaction.categoryId,
-            categoryName: categoryName,
-            amount: transaction.amount,
-            notes: transaction.notes,
-            documentationUrl: transaction.documentationUrl,
-        };
-    })
+    return {
+      id: transaction.id,
+      workspaceId: transaction.workspaceId,
+      date: transaction.date,
+      category: {
+        id: transaction.categoryId,
+        name: categoryName,
+      },
+      categoryId: transaction.categoryId,
+      categoryName: categoryName,
+      amount: transaction.amount,
+      notes: transaction.notes,
+      documentationUrl: transaction.documentationUrl,
+    };
+  });
 }
