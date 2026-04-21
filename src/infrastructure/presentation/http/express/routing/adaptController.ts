28ce818621c2075df81beb74c391698d9bff9788
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-04-21 10:52:20 +0200

    Map domain/application errors to HTTP status codes

--- src/infrastructure/presentation/http/express/routing/adaptController.ts
+++ src/infrastructure/presentation/http/express/routing/adaptController.ts
@@ -1,4 +1,10 @@
-﻿import { Request, Response, RequestHandler } from "express";
+import { Expired } from "@application/errors/Expired";
+import { Forbidden } from "@application/errors/Forbidden";
+import { NotFound } from "@application/errors/NotFound";
+import { ResourceConflict } from "@application/errors/ResourceConflict";
+import { Unauthenticated } from "@application/errors/Unauthenticated";
+import { InvalidInput as DomainInvalidInput } from "@domain/errors/InvalidInput";
+import { Request, Response, RequestHandler } from "express";
 
 type Controller = {
   handle(req: Request, res: Response): Promise<void>;
@@ -15,8 +21,44 @@ export function adaptController(controller: Controller): RequestHandler {
 }
 
 function handleError(error: unknown, res: Response) {
-  // TODO: Translate application/domain errors to http error codes and messages
+  if (error instanceof DomainInvalidInput) {
+    return res.status(400).json({
+      error: error.message,
+    });
+  }
+
+  if (error instanceof Unauthenticated) {
+    return res.status(401).json({
+      error: error.message,
+    });
+  }
+
+  if (error instanceof Forbidden) {
+    return res.status(403).json({
+      error: error.message,
+    });
+  }
+
+  if (error instanceof NotFound) {
+    return res.status(404).json({
+      error: error.message,
+    });
+  }
+
+  if (error instanceof ResourceConflict) {
+    return res.status(409).json({
+      error: error.message || "Resource conflict.",
+    });
+  }
+
+  if (error instanceof Expired) {
+    return res.status(410).json({
+      error: error.message,
+    });
+  }
+
   console.error(error);
+
   return res.status(500).json({
     error: "Internal server error",
   });
