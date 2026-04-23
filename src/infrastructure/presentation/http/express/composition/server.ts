b3667be8a28f02fc97a43cb710fd43048ff0bd07
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-04-23 12:50:32 +0200

    Replace custom CORS setup with cors middleware

--- src/infrastructure/presentation/http/express/composition/server.ts
+++ src/infrastructure/presentation/http/express/composition/server.ts
@@ -1,25 +1,19 @@
 ﻿import { createAppRouter } from "@infrastructure/presentation/http/express/composition/router";
 import { createSessionMiddleware } from "@infrastructure/presentation/http/express/composition/session";
 import express from "express";
+import cors from "cors";
 
 export function createServer(
   router: ReturnType<typeof createAppRouter>,
   sessionMiddleware: Awaited<ReturnType<typeof createSessionMiddleware>>,
 ) {
   const app = express();
-  app.set("trust proxy", 1);
-  app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
-    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type");
-    res.header("Access-Control-Allow-Credentials", "true");
 
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(204);
-    }
-
-    next();
-  });
+  app.use(cors({
+    origin: "http://localhost:8080",
+    credentials: true
+  }));
+  
   app.use(express.json());
   app.use(sessionMiddleware);
   app.use(router);
