7453be5bcdaecbb5dde035fa41ff40f699dd4095
Anesu Kafesu <122401845+anesukafesu@users.noreply.github.com>
2026-04-23 10:04:40 +0200

    Restrict CORS to localhost:8080

--- src/infrastructure/presentation/http/express/composition/server.ts
+++ src/infrastructure/presentation/http/express/composition/server.ts
@@ -8,7 +8,7 @@ export function createServer(
 ) {
   const app = express();
   app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "http://localhost:8080");
     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
     res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
 
